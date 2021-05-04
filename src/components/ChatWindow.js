69577d69b26689f9b52b96b93fc08e4ade95af01
CheeKiam Ng <cheekiam.ng@weareeverise.com>
2021-05-04 11:31:03 +0800

    chatwindows hook

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,44 +1,39 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import MessageList from './MessageList';
 import UserInput from './UserInput';
 import Header from './Header';
 
-class ChatWindow extends Component {
-  constructor(props) {
-    super(props);
-  }
+function ChatWindow(props) {
+  let messageList = props.messageList || [];
+  let classList = ['sc-chat-window', props.isOpen ? 'opened' : 'closed'];
 
-  onUserInputSubmit(message) {
-    this.props.onUserInputSubmit(message);
-  }
+  const onUserInputSubmit = (message) => {
+    props.onUserInputSubmit(message);
+  };
 
-  onFilesSelected(filesList) {
-    this.props.onFilesSelected(filesList);
-  }
+  const onFilesSelected = (filesList) => {
+    props.onFilesSelected(filesList);
+  };
 
-  render() {
-    let messageList = this.props.messageList || [];
-    let classList = ['sc-chat-window', this.props.isOpen ? 'opened' : 'closed'];
-    return (
-      <div className={classList.join(' ')}>
-        <Header
-          teamName={this.props.agentProfile.teamName}
-          imageUrl={this.props.agentProfile.imageUrl}
-          onClose={this.props.onClose}
-        />
-        <MessageList
-          messages={messageList}
-          imageUrl={this.props.agentProfile.imageUrl}
-        />
-        <UserInput
-          onSubmit={this.onUserInputSubmit.bind(this)}
-          onFilesSelected={this.onFilesSelected.bind(this)}
-          showEmoji={this.props.showEmoji}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={classList.join(' ')}>
+      <Header
+        teamName={props.agentProfile.teamName}
+        imageUrl={props.agentProfile.imageUrl}
+        onClose={props.onClose}
+      />
+      <MessageList
+        messages={messageList}
+        imageUrl={props.agentProfile.imageUrl}
+      />
+      <UserInput
+        onSubmit={onUserInputSubmit}
+        onFilesSelected={onFilesSelected}
+        showEmoji={props.showEmoji}
+      />
+    </div>
+  );
 }
 
 ChatWindow.propTypes = {
